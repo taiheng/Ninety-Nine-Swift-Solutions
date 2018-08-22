3b94d1b032a1c9dd1a18a2a8766889f677c6e5b4
Eneko Alonso <eneko.alonso@gmail.com>
2018-08-21 20:15:56 -0700

    Fix randomInt() method when limit < 2 (#32)

--- Sources/NinetyNineSwiftProblems/Random.swift
+++ Sources/NinetyNineSwiftProblems/Random.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public func randomInt(under limit: Int) -> Int {
+    if limit < 2 {
+        return 0
+    }
     #if os(Linux)
         srandom(UInt32(time(nil)))
         return random() % limit
